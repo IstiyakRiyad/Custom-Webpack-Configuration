3a58f7fe8078e34a9fd4a39e45685755d5ca7d72
IstiyakRiyad <istiyak.riyad@gmail.com>
2021-08-13 19:57:08 +0600

    Setting the mode option

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 const path = require('path');
 
+let mode = "development";
+
+
+if(process.env.NODE_ENV === "production") {
+    mode = "production";
+}
+
+
 module.exports = {
+    mode: mode,
+
     entry: {
         main: "./src/scripts/main.js"
     },
