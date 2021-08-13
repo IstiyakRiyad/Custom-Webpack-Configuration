e61fdce961457c2b64bc3422d2ecabae50095229
IstiyakRiyad <istiyak.riyad@gmail.com>
2021-08-13 18:13:11 +0600

    Adding input and output to webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: "./src/scripts/main.js"
+    },
+
+    output: {
+        filename: "main.js",
+
+        path: path.resolve(__dirname, "build")
+    }
+};
