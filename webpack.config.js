d3be08147b0992972a299a748f779c024fabc748
IstiyakRiyad <istiyak.riyad@gmail.com>
2021-08-13 20:12:03 +0600

    Adding HTML plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { Template } = require('webpack');
 
 let mode = "development";
 let tool = "source-map";
@@ -22,6 +24,10 @@ module.exports = {
         path: path.resolve(__dirname, "build"),
         clean: true
     },
+
+    plugins: [
+        new HtmlWebpackPlugin({template: "./src/template.html"})
+    ],
 
     devtool: tool,
 
